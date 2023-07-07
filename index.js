2481500c6c387567eb930d0e64a2f42d717e37e2
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-07 15:08:18 +0300

    added the first movie to the html

--- index.js
+++ index.js
@@ -7,33 +7,61 @@ function getMovies(){
     .then(resp => resp.json())
     .then(data =>{
         displayMovieNames(data)
+        fetchFetchirstMovie()
     })
 }
 
 //displays the movies onto the left side of the htll doc
 function displayMovieNames(movieName){
     let movieContainer = document.getElementById('leftDiv')
-
+    let movieList = document.getElementById('films')
 
 
     movieName.forEach(movie=> {
-        let movieDiv = document.createElement('div')
-        movieDiv.classList = "movieNameDiv"
-        movieDiv.innerHTML = `${movie.title} 
-        <br> 
-        ${movie.runtime}
-         <br> 
-        ${movie.capacity}
-        <br> 
-        ${movie.showtime} 
-        <br> 
-        ${movie.tickets_sold}
-        <br> 
-        ${movie.description}
-        <br>
-        ${movie.poster}`
-        movieContainer.appendChild(movieDiv)
-        // console.log(movieDiv)
+        let movieItem = document.createElement('li')
+        movieItem.classList = "movieNameList"
+        let availableTickets = parseInt(`${movie.capacity} - ${movie.tickets_sold}`)
+        movieItem.innerHTML = `
+            ${movie.poster}
+            <br>
+            ${movie.title} 
+            <br> 
+            ${movie.runtime}
+            <br>
+            ${movie.showtime}
+            <br> 
+            ${availableTickets}
+            <br>`
+        movieList.appendChild(movieItem)
     })
-    // console.log(movieDiv)
+    console.log(movieList)
+}
+
+//fetch the first movie on the right side by default
+function fetchFetchirstMovie(){
+    fetch(`http://localhost:3000/films/1`)
+    .then(resp => resp.json())
+    .then(data => {
+        displayFirstMovie(data)
+    })
+}
+
+//display the first movie ont0 the right side
+function displayFirstMovie(firstMovie){
+    let movieContainer = document.getElementById('rightDiv')
+    let availableTickets = parseInt(`${firstMovie.capacity} - ${firstMovie.tickets_sold}`)
+    console.log(firstMovie);
+
+    let firstMovieDetails = document.createElement('div')
+    firstMovieDetails.innerHTML = `
+        ${firstMovie.poster}
+        <br>
+        ${firstMovie.title}
+        <br>
+        ${firstMovie.runtime}
+        <br>
+        ${firstMovie.showtime}
+        <br>
+        ${availableTickets}` 
+    movieContainer.appendChild(firstMovieDetails)
 }
